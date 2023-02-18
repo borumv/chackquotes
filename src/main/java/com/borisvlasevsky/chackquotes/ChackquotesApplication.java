7b0f13d903f75d05f67a601eead12a35488291e6
Falltroy <fallTroy@protonmail.com>
2023-02-18 13:27:52 +0300

    Init project closes#1

---
+++ src/main/java/com/borisvlasevsky/chackquotes/ChackquotesApplication.java
@@ -0,0 +1,13 @@
+package com.borisvlasevsky.chackquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChackquotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChackquotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/borisvlasevsky/chackquotes/ChackquotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borisvlasevsky.chackquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChackquotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
